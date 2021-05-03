888ecf1e70fec134bd87885c6a585a1b3d904875
Astrid <33372403+GavinSyria@users.noreply.github.com>
2021-05-03 23:47:38 +0800

    加了一些動態抽取資料的函示

--- js/script.js
+++ js/script.js
@@ -12,4 +12,31 @@ $(function () { // Same as document.addEventListener("DOMContentLoaded"...
     $(event.target).focus();
   });
 });
+(function(global){
+  var od = {};
+  var homeHtml = "OD_Project/home.html";
+  // Convenience function for inserting innerHTML for 'select'
+  var insertHtml = function (selector, html) {
+    var targetElem = document.querySelector(selector);
+    targetElem.innerHTML = html;
+  };
+  
+  // Show loading icon inside element identified by 'selector'.
+  var showLoading = function (selector) {
+    var html = "<div class='text-center'>";
+    html += "<img src='images/ajax-loader.gif'></div>";
+    insertHtml(selector, html);
+  };
 
+  // Return substitute of '{{propName}}'
+  // with propValue in given 'string'
+  var insertProperty = function (string, propName, propValue) {
+    var propToReplace = "{{" + propName + "}}";
+    string = string.replace(new RegExp(propToReplace, "g"), propValue);
+    return string;
+  }
+  var inserthomeHTML = function(){
+    insertHtml("main-content",homeHtml);
+  }
+  global.$od = od;
+})(window);
