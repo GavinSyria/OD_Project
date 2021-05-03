2f2163eb0f468aaf162004ea54fe2957c2e49660
astrid chang <yugi0604@gmail.com>
2021-05-03 23:19:36 +0800

    first_update

---
+++ js/script.js
@@ -0,0 +1,15 @@
+/*global $*/
+$(function () { // Same as document.addEventListener("DOMContentLoaded"...
+
+  // Same as document.querySelector("#navbarToggle").addEventListener("blur",...
+  $("#navbarToggle").blur(function (event) {
+    var screenWidth = window.innerWidth;
+    if (screenWidth < 768) {
+      $("#collapsable-nav").collapse('hide');
+    }
+  });
+    $("#navbarToggle").click(function (event) {
+    $(event.target).focus();
+  });
+});
+
