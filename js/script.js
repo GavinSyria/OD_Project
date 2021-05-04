4b19f35769db959b1b13eef182fbfdcf9e9faabe
astrid chang <yugi0604@gmail.com>
2021-05-04 12:20:19 +0800

    加入音樂

--- js/script.js
+++ js/script.js
@@ -12,42 +12,4 @@ $(function () { // Same as document.addEventListener("DOMContentLoaded"...
     $(event.target).focus();
   });
 });
-(function(global){
-  var od = {};
-  var homeHtml = "snippets/home.html";
-  // Convenience function for inserting innerHTML for 'select'
-  var insertHtml = function (selector, html) {
-    var targetElem = document.querySelector(selector);
-    targetElem.innerHTML = html;
-  };
-  
-  // Show loading icon inside element identified by 'selector'.
-  var showLoading = function (selector) {
-    var html = "<div class='text-center'>";
-    html += "<img src='images/ajax-loader.gif'></div>";
-    insertHtml(selector, html);
-  };
 
-  // Return substitute of '{{propName}}'
-  // with propValue in given 'string'
-  var insertProperty = function (string, propName, propValue) {
-    var propToReplace = "{{" + propName + "}}";
-    string = string.replace(new RegExp(propToReplace, "g"), propValue);
-    return string;
-  }
-  /*var inserthomeHTML = function(){
-    $ajaxUtils.sendGetRequest(homeHtml,function (responseText) {
-    document.querySelector("#main-content").innerHTML = responseText;},false);
-  }*/
-  
-  document.addEventListener("DOMContentLoaded", function (event) {
-    $ajaxUtils.sendGetRequest(
-  homeHtml,
-  function (responseText) {
-    document.querySelector("#main-content")
-      .innerHTML = responseText;
-  },
-  false);
-});
-  global.$od = od;
-})(window);
